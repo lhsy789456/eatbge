37d4ccc44fdee4a0bba982f2027b42ab4ea70cfd
lhsy789456 <98519816+lhsy789456@users.noreply.github.com>
2022-01-28 17:15:17 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -435,10 +435,10 @@ function backBtn() {
 function shareText(score) {
 
     deviation_time = (date2.getTime() - _date1.getTime())
-    if (score <= 2.5 * __Time) return '加油！我相信您可以的！';
-    if (score <= 5 * __Time) return '^_^ 加把劲，底力大王就是您！';
-    if (score <= 7.5 * __Time) return '您！';
-    if (score <= 10 * __Time) return '太 您 了！';
+    if (score <= 2.5 * __Time) return '不要被逼哥瞧不起！';
+    if (score <= 5 * __Time) return '^_^ 加把劲，马上你就是新的逼哥';
+    if (score <= 7.5 * __Time) return '参见逼哥！！';
+    if (score <= 10 * __Time) return '太 牛 逼 了！';
     return '您是外星人嘛？';
 }
 
